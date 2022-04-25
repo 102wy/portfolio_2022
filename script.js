c402cdd0f71d532e664abd6e52fddbeda2cb3702
102wy <tser5@naver.com>
2022-04-26 03:57:41 +0900

    all commit

--- script.js
+++ script.js
@@ -10,4 +10,22 @@ window.onload = function () {
       header.removeClass("white");
     }
   });
+
+  const pageBtn = document.querySelectorAll(".navbtn");
+  for (let i = 0; i < pageBtn.length; i++) {
+    pageBtn[i].addEventListener("click", (e) => {
+      e.preventDefault();
+      document
+        .querySelector(".pages" + (i + 1))
+        .scrollIntoView({ behavior: "smooth" });
+    });
+  }
+
+  document.querySelector(".portfolio").addEventListener("click", (e) => {
+    e.preventDefault();
+    document.querySelector(".pages3").scrollIntoView({ behavior: "smooth" });
+  });
+  $(".more").click(function () {
+    $(".target").toggleClass("hide");
+  });
 };
